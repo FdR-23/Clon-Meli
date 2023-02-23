50461dc6a838e0f595694b4daa732b8c4c753c28
FeDe <federampi@gmail.com>
2023-02-22 21:55:52 -0300

    fix url payments

--- client/src/components/Home/PaymentsHome.jsx
+++ client/src/components/Home/PaymentsHome.jsx
@@ -10,8 +10,8 @@ const PaymentsHome = () => {
                     <div className='flex-auto flex flex-wrap'>
 
                         <div className="flex-grow basis-auto text-lg h-[90px] p-5 whitespace-nowrap">
-                            <a className='float-left h-[54px] mr-[16px] w-[48px]' data-title="Promociones bancarias">
-                                <img decoding="async" src="https://http2.mlstatic.com/storage/homes-korriban/assets/images/payments/credit-card.svg" alt="Tarjeta de crédito" />
+                            <a className='float-left h-[54px] mr-[16px] w-[48px] cursor-pointer'>
+                                <img  src="https://http2.mlstatic.com/storage/homes-korriban/assets/images/payments/credit-card.svg" alt="Tarjeta de crédito" />
                             </a>
                             <div className="inline-block mt-1px">Tarjeta de crédito</div>
                             <div className="text-[#999] text-sm mt-[-1px]">
@@ -20,7 +20,7 @@ const PaymentsHome = () => {
                         </div>
 
                         <div className="flex-grow basis-auto text-lg h-[90px] p-5 whitespace-nowrap">
-                            <a className="float-left h-[54px] mr-[16px] w-[48px]" data-title="Medios de pago">
+                            <a className="float-left h-[54px] mr-[16px] w-[48px] cursor-pointer">
                                 <img decoding="async" src="https://http2.mlstatic.com/storage/homes-korriban/assets/images/payments/debit-card.svg" alt="Tarjeta de débito" />
                             </a>
                             <div className="inline-block mt-1px">Tarjeta de débito</div>
@@ -30,23 +30,23 @@ const PaymentsHome = () => {
                         </div>
 
                         <div className="flex-grow basis-auto text-lg h-[90px] p-5 whitespace-nowrap">
-                            <a className="float-left h-[54px] mr-[16px] w-[48px]" >
-                                <img decoding="async" src="https://http2.mlstatic.com/storage/homes-korriban/assets/images/payments/mercado-creditsv2.svg" alt="Cuotas sin tarjeta" />
+                            <a className="float-left h-[54px] mr-[16px] w-[48px] cursor-pointer" >
+                                <img src="https://http2.mlstatic.com/storage/homes-korriban/assets/images/payments/mercado-creditsv2.svg" alt="Cuotas sin tarjeta" />
                             </a>
                             <div className="inline-block mt-1px">Cuotas sin tarjeta</div>
 
                             <div className="text-[#999] text-sm mt-[-1px]">
-                                <a className='text-[#3483fa] hover:text-[#1259c3]' href="https://mercadolibre.com.ar/mercado-credito/cuotas-sin-tarjeta/?sk=RHRsnBTf" >Conocé Mercado Crédito</a>
+                                <a className='text-[#3483fa] hover:text-[#1259c3] cursor-pointer'>Conocé Mercado Crédito</a>
                             </div>
                         </div>
 
                         <div className="flex-grow basis-auto text-lg h-[90px] p-5 whitespace-nowrap">
-                            <a className="float-left h-[54px] mr-[16px] w-[48px]" href="/gz/home/payments/methods#payment-agreement" data-title="Medios de pago">
+                            <a className="float-left h-[54px] mr-[16px] w-[48px] cursor-pointer">
                                 <img decoding="async" src="https://http2.mlstatic.com/storage/homes-korriban/assets/images/payments/payment-agreement.svg" alt="Efectivo" />
                             </a>
                             <div className="inline-block mt-1px">Efectivo</div>
                             <div className="text-[#999] text-sm mt-[-1px]">
-                                <a className='text-[#3483fa] hover:text-[#1259c3]' href="https://www.mercadolibre.com.ar/gz/home/payments/methods#debit-card" modal_title="Medios de pago" data-title="Medios de pago" aria-label="Efectivo, Ver más">
+                                <a className='text-[#3483fa] hover:text-[#1259c3] cursor-pointer'>
                                     Ver más</a>
                             </div>
                         </div>
@@ -54,7 +54,7 @@ const PaymentsHome = () => {
 
                     <div className="flex flex-wrap">
                         <div className="flex-grow basis-auto text-lg h-[90px] p-5 whitespace-nowrap">
-                            <a className="float-left h-[54px] mr-[16px] w-[48px] cursor-pointer" data-title="Medios de pago">
+                            <a className="float-left h-[54px] mr-[16px] w-[48px] cursor-pointer">
                                 <img decoding="async" src="https://http2.mlstatic.com/storage/homes-korriban/assets/images/payments/view-more.svg" alt="Medios de pago" />
                             </a>
                         </div>
